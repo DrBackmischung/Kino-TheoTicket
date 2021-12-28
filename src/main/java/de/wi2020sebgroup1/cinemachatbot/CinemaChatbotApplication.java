a21ed82fd2b87151bc3417f28006f3ca26375487
Neunzig <Neunzig@DESKTOP-GJFDB1H.fritz.box>
2021-12-29 00:10:54 +0100

    start maven

---
+++ src/main/java/de/wi2020sebgroup1/cinemachatbot/CinemaChatbotApplication.java
@@ -0,0 +1,13 @@
+package de.wi2020sebgroup1.cinemachatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wi2020sebgroup1/cinemachatbot/CinemaChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wi2020sebgroup1.cinemachatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
